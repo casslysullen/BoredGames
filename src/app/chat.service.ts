e9cb63aa1c8d7942c23299013485a1ec47ce4756
ivonaivezaj <59172446+ivonaivezaj@users.noreply.github.com>
2020-06-15 18:53:26 -0400

    Add files via upload

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
-import { ApiAiClient } from 'api-ai-javascript';
+import { ApiAiClient } from "api-ai-javascript/es6/ApiAiClient";
+
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
