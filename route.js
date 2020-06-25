7c973195ea32ce41872d56d0c40be1056058607a
Cassly Sullen <csulleniii@gmail.com>
2020-06-24 20:19:50 -0400

    route updated

--- route.js
+++ route.js
@@ -4,9 +4,9 @@ var http = require('http');
 
 
 app.get('/api/:term', function (req, res) {
-  var apiKey = '06140356b004cf4a23560c7cd95af899';
+  var apiKey = '0e5c263be7a96b8df205c354017ca71730b176c5';
 
-  let url = "https://api-v3.igdb.com/games/06140356b004cf4a23560c7cd95af899";
+  let url = 'https://www.giantbomb.com/api/games/?api_key=0e5c263be7a96b8df205c354017ca71730b176c5&format=json&field_list=name,image';
   var options = {
     method: "GET",
     headers: {
